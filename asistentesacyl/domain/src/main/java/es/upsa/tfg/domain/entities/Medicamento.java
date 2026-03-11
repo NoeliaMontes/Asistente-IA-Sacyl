419d9acdebdcaf715eb0b237fae0c1b065365a21
NoeliaMontes <nmontessa.inf@upsa.es>
2026-03-11 19:42:00 +0100

    Comentarios domain añadidos
    El módulo domain ha sido comentado.

--- asistentesacyl/domain/src/main/java/es/upsa/tfg/domain/entities/Medicamento.java
+++ asistentesacyl/domain/src/main/java/es/upsa/tfg/domain/entities/Medicamento.java
@@ -5,10 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//Utilizamos la librería lombok para facilitar los constructor
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+//Clase Medicamento con sus atributos
 public class Medicamento
 {
     String id;
--- asistentesacyl/domain/src/main/java/es/upsa/tfg/domain/exceptions/MedicamentoNotFoundException.java
+++ asistentesacyl/domain/src/main/java/es/upsa/tfg/domain/exceptions/MedicamentoNotFoundException.java
@@ -2,7 +2,7 @@ package es.upsa.tfg.domain.exceptions;
 
 public class MedicamentoNotFoundException extends RuntimeException
 {
-
+    //Esta excepción devuelve el mensaje indicado abajo como respuesta.
     public MedicamentoNotFoundException()
     {
         super("No poseemos ningún medicamento que concuerde con lo que está consultando.");
