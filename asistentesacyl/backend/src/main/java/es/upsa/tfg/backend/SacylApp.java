c456e165f66e79cf4b4f6d6709976e01387b793d
NoeliaMontes <nmontessa.inf@upsa.es>
2026-03-22 20:10:33 +0100

    Implementación RAG
    Se añadió los detalles de un RAG inicial para el backend (el asistente IA). Falta implementar documentos y probarlo. Por ahora solo acepta documentos en pdf.

---
+++ asistentesacyl/backend/src/main/java/es/upsa/tfg/backend/SacylApp.java
@@ -0,0 +1,31 @@
+package es.upsa.tfg.backend;
+
+import io.quarkus.runtime.QuarkusApplication;
+import jakarta.inject.Inject;
+
+import java.util.Scanner;
+
+public class SacylApp implements QuarkusApplication
+{
+
+    @Inject
+    DocumentationAsistant assistant;
+
+    @Override
+    public int run(String... args) throws Exception
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Indicame en qué puedo ayudarte");
+        String question = scanner.nextLine();
+        do {
+            String answer = assistant.askQuestion(question);
+            System.out.println(answer);
+            question = scanner.nextLine();
+        }while(!question.equals("salir"));
+
+        return 0;
+    }
+
+
+}
